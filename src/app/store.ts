b11a008d6b071f0e2524fd5cd12fb0f391d3d4d2
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-06-08 11:46:05 +0900

    feat: redux-persist 타입스크립트 적용

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,36 @@
-import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit"
+/* eslint-disable prettier/prettier */
+import {
+  configureStore,
+  ThunkAction,
+  ThunkDispatch,
+  Action,
+  combineReducers,
+  AnyAction,
+} from "@reduxjs/toolkit"
 import counterReducer from "../features/counter/counterSlice"
+import { persistReducer } from "redux-persist"
+import thunk from 'redux-thunk';
+// import storage from 'redux-persist/lib/storage'; // for local storage
+import storage from "redux-persist/lib/storage/session"; // for session storage
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+const reducers = combineReducers({
+  counter: counterReducer,
 })
 
-export type AppDispatch = typeof store.dispatch
+const persistConfig = {
+  key: "root",
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: [thunk],
+})
+
+export type AppDispatch = ThunkDispatch<RootState, unknown, AnyAction>
 export type RootState = ReturnType<typeof store.getState>
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
+/* eslint-disable prettier/prettier */
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { Provider } from "react-redux"
 import { store } from "./app/store"
 import App from "./App"
 import "./index.css"
+import { PersistGate } from "redux-persist/integration/react"
+import { persistStore } from "redux-persist"
 
+const persistor = persistStore(store)
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
